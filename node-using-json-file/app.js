c255f350967c650262d26d84599fa91226e682f9
parththakkar06 <parththakkar1013@gmail.com>
2026-02-03 12:05:09 +0530

    cors

--- node-using-json-file/app.js
+++ node-using-json-file/app.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
+const corsOptions = {
+    origin : "http://localhost:3000",
+    methods : ["GET","POST"],
+    allowedHeaders : ['Content-type', 'Authorization']
+}
 // global middleware
 app.use(express.json())
+app.use(cors(corsOptions))
 
 
 const userRoutes = require("./routes/UserRoutes")
