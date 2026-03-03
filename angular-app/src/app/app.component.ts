879d1e8b5a4214904aa2fae3f0aa9ff82d5aadb5
parththakkar06 <parththakkar1013@gmail.com>
2026-03-03 18:42:20 +0530

    Computed signals

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
@@ -129,43 +129,67 @@ export class AppComponent {
   // }
 
 
-  users = ['Ram', 'Shyam', 'Raj', 'Rahul', 'Rudra']
+  // users = ['Ram', 'Shyam', 'Raj', 'Rahul', 'Rudra']
 
-  students = [
-    { name: 'Ram', age: 20, email: "Ram@gmail.com" },
-    { name: 'Shyam', age: 20, email: "Shyam@gmail.com" },
-    { name: 'Raj', age: 20, email: "Raj@gmail.com" },
-    { name: 'Rahul', age: 20, email: "Rahul@gmail.com" },
-    { name: 'Rudra', age: 20, email: "Rudra@gmail.com" }
-  ]
+  // students = [
+  //   { name: 'Ram', age: 20, email: "Ram@gmail.com" },
+  //   { name: 'Shyam', age: 20, email: "Shyam@gmail.com" },
+  //   { name: 'Raj', age: 20, email: "Raj@gmail.com" },
+  //   { name: 'Rahul', age: 20, email: "Rahul@gmail.com" },
+  //   { name: 'Rudra', age: 20, email: "Rudra@gmail.com" }
+  // ]
 
-  getName(name: string) {
-    console.log(name)
+  // getName(name: string) {
+  //   console.log(name)
+  // }
+
+  // handleEvent(event: Event) {
+  //   // console.log("function called" , event.type);
+  //   console.log((event.target as HTMLInputElement).value);
+
+  // }
+
+
+  // data = 100
+  // count = signal(10)
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log(this.count());
+
+  //   })
+  // }
+
+  // updateValue(val: string) {
+  //   // this.count.set(20)
+  //   if(val == 'inc'){
+  //     this.count.set(this.count()+1)
+  //   }else{
+  //     this.count.set(this.count()-1)
+  //   }
+  // }
+
+  // data = signal<number | string>(10)
+  data:WritableSignal<number> = signal(10)
+  coun : Signal<number> =computed(()=>20)
+
+  updateValue(){
+    // this.data.set("ello")
+    this.data.update(val => val + 10)
   }
 
-  handleEvent(event: Event) {
-    // console.log("function called" , event.type);
-    console.log((event.target as HTMLInputElement).value);
 
+  x = signal(20)
+  y = signal(30)
+  z = computed(()=>this.x() + this.y())
+
+  updateValues(){
+    console.log(this.z());
+    this.x.set(100)
+    console.log(this.z())
   }
 
-
-  data = 100
-  count = signal(10)
-
-  constructor() {
-    effect(() => {
-      console.log(this.count());
-
-    })
-  }
-
-  updateValue(val: string) {
-    // this.count.set(20)
-    if(val == 'inc'){
-      this.count.set(this.count()+1)
-    }else{
-      this.count.set(this.count()-1)
-    }
+  updateXValue(){
+    this.x.set(1000)
   }
 }
