b8e384ebea0e7c85a372eb9897e8f7bf0cc841ee
parththakkar06 <parththakkar1013@gmail.com>
2026-03-03 11:53:57 +0530

    angular start

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SignupComponent, CommonModule, LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-app';
+  // count = 0;
+  // increase() {
+  //   this.count++;
+  // }
+  // decrease() {
+  //   this.count--;
+  // }
+
+  // isVisible = true
+  // toggle(){
+  //   this.isVisible = !this.isVisible;
+  // }
+
+
+  // name = "Parth"
+  // age = 20
+
+  // increaseAge() {
+  //   this.age++;
+  //   console.log("Clicked")
+  // }
+
+  // resetAge() {
+  //   this.age = 20;
+  // }
+
+  // numbers = [1, 2, 3]
+
+  // getSum() {
+  //   console.log("Calculating...")
+  //   return this.numbers.reduce((a, b) => a + b, 0)
+  // }
+
+  // isDisabled = true;
+  // imageUrl = "https://in.pinterest.com/pin/artistic-wallpaper--621496817359389265/";
+
+
+  typedValue = ''
+  onInput(event : Event){
+    const input = event.target as HTMLInputElement
+    this.typedValue = input.value
+  }
+}
