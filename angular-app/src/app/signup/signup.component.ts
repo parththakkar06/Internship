77e5af1b25960bbab484f2db74e5de38d92c43c1
parththakkar06 <parththakkar1013@gmail.com>
2026-03-10 19:00:40 +0530

    Reactive Forms Validation

--- angular-app/src/app/signup/signup.component.ts
+++ angular-app/src/app/signup/signup.component.ts
@@ -1,17 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
   profileForm = new FormGroup({
-    name :new FormControl('joey'),
-    password : new FormControl('123'),
-    email : new FormControl('joey@test.com')
+    name :new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required,Validators.minLength(5)]),
+    email : new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')])
   })
 
   onSubmit(){
@@ -19,11 +20,22 @@ export class SignupComponent {
     
   }
 
-  setValue(){
-    this.profileForm.setValue({
-      name : 'alice',
-      password : '12321',
-      email : 'alice@test.com'
-    })
+  get name(){
+    return this.profileForm.get('name')
   }
+
+  get password(){
+    return this.profileForm.get('password')
+  }
+
+  get email(){
+    return this.profileForm.get('email')
+  }
+  // setValue(){
+  //   this.profileForm.setValue({
+  //     name : 'alice',
+  //     password : '12321',
+  //     email : 'alice@test.com'
+  //   })
+  // }
 }
