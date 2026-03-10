95998bd8f763c33a79f42a656869499ee03ac5e6
parththakkar06 <parththakkar1013@gmail.com>
2026-03-10 18:30:13 +0530

    form grouping in reactive forms

--- angular-app/src/app/signup/signup.component.ts
+++ angular-app/src/app/signup/signup.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  profileForm = new FormGroup({
+    name :new FormControl('joey'),
+    password : new FormControl('123'),
+    email : new FormControl('joey@test.com')
+  })
 
+  onSubmit(){
+    console.log(this.profileForm.value);
+    
+  }
+
+  setValue(){
+    this.profileForm.setValue({
+      name : 'alice',
+      password : '12321',
+      email : 'alice@test.com'
+    })
+  }
 }
