4a4b6008e3859d015af3811aff3b30fa7bcdbe42
parththakkar06 <parththakkar1013@gmail.com>
2026-03-10 18:11:36 +0530

    reactive form basic

--- angular-app/src/app/login/login.component.ts
+++ angular-app/src/app/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  name = new FormControl('212asasd')
+  password = new FormControl('111')
 
+  displayValue(){
+    console.log(this.name.value)
+    console.log(this.password.value)
+  }
+
+  setValues(){
+    this.name.setValue('spidey')
+    this.password.setValue('ayooooooooo')
+  }
 }
