6d83a1dc7020e875f324dc9a358ec799497abcd5
parththakkar06 <parththakkar1013@gmail.com>
2026-03-11 17:27:53 +0530

    custom pipe

---
+++ angular-app/src/app/pipe/currency-convertor.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currencyConvertor'
+})
+export class CurrencyConvertorPipe implements PipeTransform {
+
+  transform(value: number, ...args: number[]): unknown {
+    if(args.length>0){
+      let [data] = args 
+      return value*data;
+    }else{
+      return value*93;
+    }
+  }
+
+}
--- angular-app/src/app/profile/profile.component.ts
+++ angular-app/src/app/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CurrencyConvertorPipe } from '../pipe/currency-convertor.pipe';
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyConvertorPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
